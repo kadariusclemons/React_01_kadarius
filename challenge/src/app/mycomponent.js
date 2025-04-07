4ba213ba3590da786417af48e5253f890da9ab96
kadariusclemons <clemonskadarius@gmail.com>
2025-04-07 11:19:08 -0500

    Updating

--- challenge/src/app/mycomponent.js
+++ challenge/src/app/mycomponent.js
@@ -1,9 +1,9 @@
 export default function MyComponent() {
     return(
         <>
-            <h1>Hello, Class!</h1>
-            <p>Let's write some React!</p>
-            <img src="/bhack24.png" alt="hacker"></img>
+            <h1>Hello, Flo!</h1>
+            <p>North American Tour 2025!</p>
+            <img src="/flobanner.webp" alt="flo"></img>
         </>
     );
 }
